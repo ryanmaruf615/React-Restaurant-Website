f24a6e2bc79b47760463849755e4959fd7495ec2
Md. Maruf Hossain <ryanmaruf615@gmail.com>
2020-06-30 00:22:48 +0600

    "Second commit"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from "reactstrap";
+import MenuComponents from "./components/MenuComponents";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
             <NavbarBrand href="/">Fusion Coffe House</NavbarBrand>
         </div>
       </Navbar>
+        <MenuComponents />
     </div>
   );
 }
