ca3f0816466bbcca6478c3cb34dd04baef649116
Md Maruf Hossain <37552845+ryanmaruf615@users.noreply.github.com>
2020-08-16 23:04:48 +0600

    Add files via upload
    JSON Server

---
+++ json-server/public/precache-manifest.4392a79a6d0ef96e9c3f8f30bc8ce76d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3354104f196576b8fd7a413dbf5a636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "237df73a9df57951fd5c",
+    "url": "/static/css/2.c21e1fb8.chunk.css"
+  },
+  {
+    "revision": "89e12c5b50e76cc268dd",
+    "url": "/static/css/main.1d631852.chunk.css"
+  },
+  {
+    "revision": "237df73a9df57951fd5c",
+    "url": "/static/js/2.896e7d81.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.896e7d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e12c5b50e76cc268dd",
+    "url": "/static/js/main.0e3b220c.chunk.js"
+  },
+  {
+    "revision": "751e9daf68b8f9b25129",
+    "url": "/static/js/runtime-main.ebd1611c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
